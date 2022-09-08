58ae56e27c6fe7bf9a565cd6d47948e915c6aae8
XiaoLi_1456 <XiaoLi_1456@outlook.com>
2022-09-08 14:20:09 +0800

    first init

---
+++ start/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <h1>你好</h1>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped lang="scss">
+html,
+body {
+  width: 100vw;
+  height: 100vh;
+  margin: 0;
+  #app {
+    height: 100%;
+    width: 100%;
+  }
+}
+</style>
